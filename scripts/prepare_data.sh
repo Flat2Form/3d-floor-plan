913e25b190caf3de631d36eede0b06363d5a4d4b
toctoce <toctoce@naver.com>
2025-06-07 15:10:08 +0900

    feat(dataset): prepare_data_inst.py 파일 수정 및 단일 파일 처리 기능 추가

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,3 +1,7 @@
-cd dataset/scannetv2
-python3 prepare_data_inst.py
-cd ../..
+#!/usr/bin/env bash
+
+FILE_NAME=$1  # 예: scaniverse-model 62
+
+# cd dataset/scannetv2
+python3 dataset/scannetv2/prepare_data_inst.py --file_name "$FILE_NAME"
+# cd ../..
