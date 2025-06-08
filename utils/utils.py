76d5fdd1e5671a43187e638f123998f8da028971
toctoce <toctoce@naver.com>
2025-06-09 01:34:10 +0900

    feat(utils): 포인트 클라우드와 레이블을 기반으로 AABB 리스트 생성 함수 추가

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import open3d as o3d
 import numpy as np
-from make_wall import get_wall_vertices
+import os
+import argparse
 
 def save_pcd(pcd, path):
     o3d.io.write_point_cloud(path, pcd)
@@ -57,6 +58,42 @@ def obb_to_pcd(obb):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(sampled_pts)
     return pcd
+
+def create_aabb_list(pcd, instance_labels):
+    """포인트 클라우드와 레이블을 입력받아 바운딩 박스 리스트를 생성하는 함수
+    Args:
+        pcd: 포인트 클라우드 객체 (open3d.geometry.PointCloud)
+        labels: 각 포인트의 레이블 (길이 N의 numpy 배열)
+    Returns:
+        obb_list: 바운딩 박스 객체들의 리스트 (list[open3d.geometry.OrientedBoundingBox])
+    """
+    coords = np.asarray(pcd.points)
+    aabb_list = []
+    
+    # 각 고유한 레이블에 대해 처리
+    for label in np.unique(instance_labels):
+        if label < 0:  # 유효하지 않은 레이블 무시
+            continue
+            
+        # 현재 레이블에 해당하는 포인트들 추출
+        mask = instance_labels == label
+        label_coords = coords[mask]
+        
+        # 포인트가 없는 경우 건너뛰기
+        if len(label_coords) == 0:
+            continue
+            
+        # 포인트 클라우드 생성
+        label_pcd = o3d.geometry.PointCloud()
+        label_pcd.points = o3d.utility.Vector3dVector(label_coords)
+        
+        # 바운딩 박스 생성 및 색상 설정
+        aabb = label_pcd.get_axis_aligned_bounding_box()
+        aabb.color = (1, 0, 0)  # 기본 색상 설정
+        
+        aabb_list.append(aabb)
+    
+    return aabb_list
 
 def create_obb_list(pcd, instance_labels):
     """포인트 클라우드와 레이블을 입력받아 바운딩 박스 리스트를 생성하는 함수
